f5c4cf95f495dfe29b4879a87601cf40493f095f
Sam Rae <samuelrae@gmail.com>
2015-09-01 12:34:44 +0100

    first commit for boris bikes classwork

---
+++ wk4/tdd/boris_bikes/spec/bike_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../lib/bike'
+
+# describing functionality of the specific class Bike
+describe Bike do
+
+  it 'should not be broken after we create it' do
+    bike = Bike.new # instantiate an instance of Bike class
+    expect(bike.broken?).to be false
+  end
+
+  it 'should be able to break' do
+    bike= Bike.new
+    bike.break
+    expect(bike.broken?).to be true
+  end
+
+  it 'should be repairable' do
+    bike= Bike.new
+    bike.break
+    bike.fix
+    expect(bike.broken?).to be false
+  end
+
+
+end
