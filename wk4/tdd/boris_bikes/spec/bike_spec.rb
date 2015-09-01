7f821b7d592610c55944be9be1a1ef8d73c35e7a
Sam Rae <samuelrae@gmail.com>
2015-09-01 12:40:07 +0100

    refactored bike spec, Boris Bikes

--- wk4/tdd/boris_bikes/spec/bike_spec.rb
+++ wk4/tdd/boris_bikes/spec/bike_spec.rb
@@ -3,19 +3,21 @@ require_relative '../lib/bike'
 # describing functionality of the specific class Bike
 describe Bike do
 
+  let (:bike) { Bike.new }
+
   it 'should not be broken after we create it' do
-    bike = Bike.new # instantiate an instance of Bike class
+    # bike = Bike.new # instantiate an instance of Bike class
     expect(bike.broken?).to be false
   end
 
   it 'should be able to break' do
-    bike= Bike.new
+    # bike= Bike.new
     bike.break
     expect(bike.broken?).to be true
   end
 
   it 'should be repairable' do
-    bike= Bike.new
+    # bike= Bike.new
     bike.break
     bike.fix
     expect(bike.broken?).to be false
